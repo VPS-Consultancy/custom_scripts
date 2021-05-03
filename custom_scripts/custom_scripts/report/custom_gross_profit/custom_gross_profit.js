64fc1450eca37cb76d30d73f9ba498397bf796f5
Mohammed Haris <mohammedharis.k.01@gmail.com>
2021-05-03 23:25:09 +0530

    Custom gross report -1

---
+++ custom_scripts/custom_scripts/report/custom_gross_profit/custom_gross_profit.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2016, C.R.I.O and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Custom Gross Profit"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date")
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date")
+		},
+		{
+			"fieldname":"group_by",
+			"label": __("Group By"),
+			"fieldtype": "Select",
+			"options": "Invoice\nItem Code\nItem Group\nBrand\nWarehouse\nCustomer\nCustomer Group\nTerritory\nSales Person\nProject",
+			"default": "Invoice"
+		},
+	]
+};
