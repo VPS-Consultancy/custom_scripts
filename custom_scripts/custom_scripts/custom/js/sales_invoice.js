2220ea13b4aaa7a52f46d00d1c2cedafd4fdff30
Mohammed Haris <mohammedharis.k.01@gmail.com>
2021-07-01 23:33:33 +0530

    feat (sms): send sms on submit of SI

--- custom_scripts/custom_scripts/custom/js/sales_invoice.js
+++ custom_scripts/custom_scripts/custom/js/sales_invoice.js
@@ -1,13 +1,39 @@
-{% include 'erpnext/accounts/doctype/sales_invoice/sales_invoice.js' %};
-
-frappe.ui.form.on('Sales Invoice', {
-    refresh: function(frm){
-        if(frm.doc.docstatus===1){
-        this.frm.page.add_menu_item(__('Notify Customer'), function() { 
-            frm.trigger('send_sms');});
-        }
-    },
-
-    send_sms: function() {
+frappe.ui.form.on("Sales Invoice", {
+  refresh: function (frm) {
+    console.log("ertyuioigfdfghjklkjh gcfgyuiodichv");
+    if (frm.doc.docstatus == 1) {
+      frm.add_custom_button("Notify Customer", function () {
+        frm.trigger("notify_customer");
+      });
     }
-})  
+  },
+
+  on_submit: function (frm) {
+    frm.trigger("notify_customer");
+  },
+  notify_customer: function (frm) {
+    frappe.call({
+      method: "custom_scripts.custom_scripts.custom.sales_invoice.send_sms",
+      args: {
+        customer: frm.doc.customer,
+        invoice_no: frm.doc.name,
+        due_date: frm.doc.posting_date,
+        amount: frm.doc.rounded_total,
+      },
+      freeze: true,
+      callback: function (r) {
+        if (r.message) {
+          frappe.show_alert({
+            message: __("Message sent successfully"),
+            indicator: "green",
+          });
+        } else {
+          frappe.show_alert({
+            message: __("Message sending failed"),
+            indicator: "red",
+          });
+        }
+      },
+    });
+  },
+});
--- custom_scripts/custom_scripts/custom/sales_invoice.py
+++ custom_scripts/custom_scripts/custom/sales_invoice.py
@@ -1,31 +1,52 @@
 from __future__ import unicode_literals
-import frappe
+import frappe, json
+import requests
+from erpnext.accounts.party import get_dashboard_info
+
 
 def get_mobile_number(customer):
-	
-	customer_mobile_no = frappe.db.sql("""select
-		`tabContact`.mobile_no
-	from
-		`tabContact`, `tabDynamic Link`
-	where
+
+    customer_mobile_no = frappe.db.sql(
+        """
+		SELECT `tabContact`.mobile_no
+		FROM `tabContact`, `tabDynamic Link`
+		WHERE
 		`tabDynamic Link`.parent = `tabContact`.name and
 		`tabDynamic Link`.parenttype = 'Contact' and
 		`tabDynamic Link`.link_doctype = 'Customer' and
-		`tabDynamic Link`.link_name = %(customer)s""", {"customer": customer})
-	
-	return customer_mobile_no
+		`tabDynamic Link`.link_name = %(customer)s""",
+        {"customer": customer},
+        as_dict=True,
+    )
 
+    return customer_mobile_no
+
+
+def sms_gateway(user, password, sender_id, mobile_number, message):
+    url = f"http://admagister.net/api/mt/SendSMS?user={user}&password={password}&senderid={sender_id}&channel=Trans&DCS=0&flashsms=0&number={mobile_number}&text={message}&route=6"
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers)
+    return response
+
+
+@frappe.whitelist()
 def send_sms(customer, invoice_no, due_date, amount):
-	mobile_number = get_mobile_number(customer)
-	settings = frappe.get_single('Nirmala Settings')
-	user = settings.user
-	senderid = senderid
-	password = frappe.utils.password.get_decrypted_password('Nirmala Settings', settings.name, fieldname='password')
-	mobile_number = '91'+mobile_number
-	message = f'Dear Customer, Invoice No. {invoice_no} of Rs.{amount} was generated. Due Date: {due_date}'
-	url = f"http://admagister.net/api/mt/SendSMS?user={user}&password={password}&senderid={senderid}&channel=Trans&DCS=0&flashsms=0&number={mobile_number}&text={message}&route=6"
-	headers = {
-		"Content-Type": "application/json"
-	}
-	response = requests.request("GET", url, headers=headers)
-	return response
+    mobile_number = (get_mobile_number(customer))[0]["mobile_no"]
+    settings = frappe.get_single("Nirmala Settings")
+    user = settings.user
+    sender_id = settings.sender_id
+    password = frappe.utils.password.get_decrypted_password(
+        "Nirmala Settings", settings.name, fieldname="password"
+    )
+    mobile_number = "91" + mobile_number
+    outstanding_amt = (get_dashboard_info("Customer", customer))[0]["total_unpaid"]
+    message = f"Nirmala Enterprises Alert. Dear Customer, \
+				Invoice No. {invoice_no} of Rs.{amount} \
+				was generated on {due_date}. \
+				Total outstanding amount as of today is {outstanding_amt} ."
+    result = sms_gateway(user, password, sender_id, mobile_number, message)
+    if result.status_code == 200:
+        res_json = json.loads(result.text)
+        if res_json["ErrorCode"] == "000":
+            return True
+    return False
