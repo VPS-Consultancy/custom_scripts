756fd4bdd46b086fbcb6f20cef3d2ce6beef1b68
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-07-03 13:23:11 +0530

    update create log function

--- custom_scripts/custom_scripts/custom/sales_invoice.py
+++ custom_scripts/custom_scripts/custom/sales_invoice.py
@@ -6,8 +6,8 @@ from erpnext.accounts.party import get_dashboard_info
 
 def get_mobile_number(customer):
 
-    customer_mobile_no = frappe.db.sql(
-        """
+	customer_mobile_no = frappe.db.sql(
+		"""
 		SELECT `tabContact`.mobile_no
 		FROM `tabContact`, `tabDynamic Link`
 		WHERE
@@ -15,35 +15,47 @@ def get_mobile_number(customer):
 		`tabDynamic Link`.parenttype = 'Contact' and
 		`tabDynamic Link`.link_doctype = 'Customer' and
 		`tabDynamic Link`.link_name = %(customer)s""",
-        {"customer": customer},
-        as_dict=True,
-    )
+		{"customer": customer},
+		as_dict=True,
+	)
 
-    return customer_mobile_no
+	return customer_mobile_no
 
+def create_log(doc_name, api_method, request, response):
+	request_log = frappe.get_doc({
+		"doctype": "SMS Request Log",
+		"user": frappe.session.user,
+		"reference_document":doc_name,
+		"api_method": api_method,
+		"response": response,
+		"request": request
+	})
+	request_log.save(ignore_permissions=True)
+	frappe.db.commit()
 
-def sms_gateway(user, password, sender_id, mobile_number, message):
-    url = f"http://admagister.net/api/mt/SendSMS?user={user}&password={password}&senderid={sender_id}&channel=Trans&DCS=0&flashsms=0&number={mobile_number}&text={message}&route=6"
-    headers = {"Content-Type": "application/json"}
-    response = requests.request("GET", url, headers=headers)
-    return response
-
+def sms_gateway(user, password, sender_id, mobile_number, message, doc_name):
+	url = f"http://admagister.net/api/mt/SendSMS?user={user}&password={password}&senderid={sender_id}&channel=Trans&DCS=0&flashsms=0&number={mobile_number}&text={message}&route=6"
+	headers = {"Content-Type": "application/json"}
+	response = requests.request("GET", url, headers=headers)
+	request_params = f'User: {user}, Sender ID: {sender_id}, Mobile No: {mobile_number}, Message: {message}'
+	create_log(doc_name, 'Send SMS', request_params, response)
+	return response
 
 @frappe.whitelist()
 def send_sms(customer, invoice_no, due_date, amount):
-    mobile_number = (get_mobile_number(customer))[0]["mobile_no"]
-    settings = frappe.get_single("Nirmala Settings")
-    user = settings.user
-    sender_id = settings.sender_id
-    password = frappe.utils.password.get_decrypted_password(
-        "Nirmala Settings", settings.name, fieldname="password"
-    )
-    mobile_number = "91" + mobile_number
-    outstanding_amt = (get_dashboard_info("Customer", customer))[0]["total_unpaid"]
-    message = f"Nirmala Enterprises Alert. Dear Customer, Invoice No. {invoice_no} of Rs.{amount} was generated on {due_date}. Total outstanding amount as of today is {outstanding_amt} ."
-    result = sms_gateway(user, password, sender_id, mobile_number, message)
-    if result.status_code == 200:
-        res_json = json.loads(result.text)
-        if res_json["ErrorCode"] == "000":
-            return True
-    return False
+	mobile_number = (get_mobile_number(customer))[0]["mobile_no"]
+	settings = frappe.get_single("Nirmala Settings")
+	user = settings.user
+	sender_id = settings.sender_id
+	password = frappe.utils.password.get_decrypted_password(
+		"Nirmala Settings", settings.name, fieldname="password"
+	)
+	mobile_number = "91" + mobile_number
+	outstanding_amt = (get_dashboard_info("Customer", customer))[0]["total_unpaid"]
+	message = f"Nirmala Enterprises Alert. Dear Customer, Invoice No. {invoice_no} of Rs.{amount} was generated on {due_date}. Total outstanding amount as of today is {outstanding_amt} ."
+	result = sms_gateway(user, password, sender_id, mobile_number, message, invoice_no)
+	if result.status_code == 200:
+		res_json = json.loads(result.text)
+		if res_json["ErrorCode"] == "000":
+			return True
+	return False
