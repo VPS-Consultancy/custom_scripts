8e40548c8f47a7ebf5919f5059e477d6a058e9e6
root <kaviyaperiyasamy22@gmail.com>
2021-10-22 17:29:38 +0000

    calculations updated

--- custom_scripts/custom_scripts/report/daily_summary/daily_summary.js
+++ custom_scripts/custom_scripts/report/daily_summary/daily_summary.js
@@ -21,10 +21,10 @@ frappe.query_reports["Daily Summary"] = {
 			on_change:  function(){
 
 				var date = frappe.query_report.get_filter_value('cf_date')
-				frappe.db.exists("Daily Balance",`DB-${frappe.datetime.add_days(date, -1)}`)
+				frappe.db.exists("Daily Summary Balance",`DSB-${frappe.datetime.add_days(date, -1)}`)
 				.then(r => {
 					if(r){
-					frappe.db.get_value("Daily Balance", {"date": frappe.datetime.add_days(date, -1)}, "amount", 
+					frappe.db.get_value("Daily Summary Balance", {"date": frappe.datetime.add_days(date, -1)}, "amount", 
 					(r1) => {
 						frappe.query_report.set_filter_value('cf_opening_balance',r1.amount) });
 					}else{
